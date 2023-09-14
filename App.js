2e8f21e7db13a9fe85684c6677c152fe066f7593
Сайлаубеков Талгат Ерханулы <SaylaubekovT@lcoast.itstep>
2023-09-14 17:36:33 +0600

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,53 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from "react";
+import { Button, Image, View, Platform, ScrollView } from "react-native";
+import * as ImagePicker from "expo-image-picker";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export default function App() {
+  const [images, setImages] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      const images = await AsyncStorage.getItem("images");
+      if (images === null) {
+        setImages([]);
+      } else {
+        setImages(JSON.parse(images));
+      }
+      console.log(images);
+    })();
+  }, []);
+
+  const pickImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      const uri = result.assets[0].uri;
+      const newImages = [...images, uri];
+      setImages(newImages);
+      await AsyncStorage.setItem("images", JSON.stringify(newImages));
+    }
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ScrollView>
+      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+        <Button title="Pick an image from camera roll" onPress={pickImage} />
+        {images.map((image, index) => {
+          return (
+            <Image
+              key={index}
+              source={{ uri: image }}
+              style={{ width: 200, height: 200 }}
+            />
+          );
+        })}
+      </View>
+    </ScrollView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
